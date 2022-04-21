cd151b7da6f3aa81ebd5ea0e062ee6a073e4aaf0
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-04-21 22:43:51 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+for (const ingredient of ingredients) {
+  const link = document.createElement("li");
+  link.textContent = ingredient;
+  link.classList.add("item");
+  console.log(link);
+  const list = document.querySelector("ul");
+  list.appendChild(link);
+}
