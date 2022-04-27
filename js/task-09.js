9c8675c8338979801625f8d82371bd24268864e6
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-04-28 02:09:30 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const buttonChange = document.querySelector("button.change-color");
+buttonChange.addEventListener("click", bodyColor);
+function bodyColor() {
+  const bodyOfDoc = document.querySelector("body");
+  bodyOfDoc.style.backgroundColor = getRandomHexColor();
+  const bgColor = document.querySelector("span.color");
+  
+  bgColor.textContent = bodyOfDoc.getAttribute("style");
+  
 }
